77e9fa6be6c1e3a1e99c893ae50ded5b07da204e
BigTsung <yutsungchiu@gmail.com>
2020-05-06 22:45:16 +0800

    create simple gun and bullet prefabs

---
+++ Assets/Scripts/Gameplay/Gun.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private float shootingSpeed = 0.25f;
+
+    private Transform mTransform;
+
+    private void Start()
+    {
+        mTransform = this.transform;
+        InvokeRepeating("Fire", 0, shootingSpeed);
+    }
+
+    private void Fire()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, mTransform.position, mTransform.rotation);
+        //Debug.Log(mTransformm.forward);
+        bullet.GetComponent<SimpleBullet>().Init(mTransform.forward);
+    }
+
+    //void Update()
+    //{
+
+    //}
+}
---
+++ Assets/Scripts/Gameplay/SimpleBullet.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class SimpleBullet : MonoBehaviour
+{
+    [SerializeField] private float life = 5f;
+    [SerializeField] private float speed = 10f;
+
+    private Rigidbody bulletRigidbody;
+
+
+    //private void Awake()
+    //{
+
+    //}
+
+    private void Start()
+    {
+        Invoke("ReadyToDie", life);
+    }
+
+    private void ReadyToDie()
+    {
+        Destroy(this.gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(this.gameObject);
+    }
+
+    public void Init(Vector3 direction)
+    {
+        if(bulletRigidbody == null)
+            bulletRigidbody = GetComponent<Rigidbody>();
+
+        bulletRigidbody.AddForce(direction * speed);
+    }
+}
