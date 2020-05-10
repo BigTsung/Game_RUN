3a9f63c24ebd8193a049071a0edcc2e87cd44c23
BigTsung <yutsungchiu@gmail.com>
2020-05-10 15:59:49 +0800

    create a simple enemySpawner

---
+++ Assets/Scripts/Gameplay/EnemySpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : Singleton<EnemySpawner>
+{
+    [SerializeField] private GameObject meleeEnemyPrfab;
+    [SerializeField] private float spawnSpeed = 1f;
+
+    private void Start()
+    {
+        InvokeRepeating("Spawn", 0, spawnSpeed);    
+    }
+
+    private void Spawn()
+    {
+        Instantiate(meleeEnemyPrfab, this.transform);
+    }
+}
